937d4b2ca089dc26422fc031e9aa0a01e7c4bac3
Vishrut19 <vishrutagarwalla19@gmail.com>
2023-07-24 10:26:10 +0530

    Added Border around Map

--- src/components/UserSection.jsx
+++ src/components/UserSection.jsx
@@ -30,7 +30,7 @@ const UserSection = () => {
             </div>
           </div>
           <iframe
-            className="w-full h-[250px] border-0"
+            className="w-full h-[250px] border-0 rounded-sm"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2522.78075150076!2d6.097925576315683!3d50.77963776360746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0995d36ff22ff%3A0x3a4248b50ce934de!2sJ%C3%BClicher%20Str.%2072a%2C%2052070%20Aachen%2C%20Germany!5e0!3m2!1sen!2sin!4v1689597649368!5m2!1sen!2sin"
             title="Google Maps Embed"
             loading="lazy"
