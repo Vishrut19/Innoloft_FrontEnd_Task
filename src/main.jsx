1a130262332ace90c366a6e1c9813a2790a5b9ad
Vishrut19 <vishrutagarwalla19@gmail.com>
2023-10-13 11:32:12 +0530

    Changed Folder Structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import store from "./Redux/store.js";
+import store from "./utils/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
